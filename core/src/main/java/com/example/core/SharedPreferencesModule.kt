ecd4348e8e49e185cd35cc57bec9bda3cb74d955
egbuna <egbunachukwudi49@gmail.com>
2019-12-12 09:14:03 +0100

    feat: core module setup

---
+++ core/src/main/java/com/example/core/SharedPreferencesModule.kt
@@ -0,0 +1,15 @@
+package com.example.core
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+
+@Module
+class SharedPreferencesModule(val context: Context, val name: String) {
+    
+    @Provides
+    fun provideSharedPreferences(): SharedPreferences {
+        return context.applicationContext.getSharedPreferences(name, Context.MODE_PRIVATE)
+    }
+}
